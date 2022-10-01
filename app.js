901d50a2dc88473472380205d2ce953db4b89dbd
Yulsan Oh <yulsanohDev@gmail.com>
2022-10-02 02:43:13 +0900

    Update complete bcrypt and password hasing practice

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const app = express();
 const User = require("./models/user");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "notagoodsecret" }));
+
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  next();
+};
 
 async function main() {
   mongoose.connect("mongodb://localhost:27017/authDemo");
@@ -25,8 +34,12 @@ app.get("/", (req, res) => {
   res.send("this is homepage");
 });
 
-app.get("/secret", (req, res) => {
-  res.send("This is Secret! You cannot see me unless you are logged in");
+app.get("/secret", requireLogin, (req, res) => {
+  res.render("secret");
+});
+
+app.get("/topsecret", requireLogin, (req, res) => {
+  res.send("TOP SECRET!!");
 });
 
 app.get("/register", (req, res) => {
@@ -35,12 +48,12 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { password, username } = req.body;
-  const hash = await bcrypt.hash(password, 12);
   const user = new User({
     username,
     password: hash,
   });
   await user.save();
+  req.session.user_id = user._id;
   res.redirect("/login");
 });
 
@@ -50,13 +63,18 @@ app.get("/login", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username: username });
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (validPassword) {
-    res.send("YAY WELCOME!");
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
+    res.redirect("/secret");
   } else {
-    res.send("TRY AGAIN");
+    res.redirect("/login");
   }
+});
+
+app.post("/logout", async (req, res) => {
+  req.session.user_id = null;
+  res.redirect("/login");
 });
 
 app.listen(3000, () => {
